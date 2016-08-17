acc94ca80c8c4a18c83b4ce63d6dc45ff055715e
JOHN SHAFF <JOHNSHAFF@GMAIL.COM>
2016-08-16 22:47:30 -0700

    forgot to new branch

---
+++ app.js
@@ -0,0 +1,41 @@
+var movies = [];
+
+function Movie (obj) {
+  this.title = obj.title;
+  this.year = obj.year;
+  this.imdb = obj.imdb;
+  this.synopsis = obj.synopsis;
+};
+
+//This clones my template and then replaces data from objects
+//Removes the class from that clone so it's not grabbed again.
+//And finally returns the entire new section for appending.
+Movie.prototype.articleMaker = function() {
+  var $newMovie = $('article.template').clone();
+  $newMovie.find('h1').text(this.title);
+  $newMovie.find('h4').text(this.year);
+  $newMovie.find('a').attr('href', this.imdb);
+  $newMovie.find('p').text(this.synopsis);
+
+  $newMovie.removeClass();
+  return $newMovie;
+};
+
+//This sorts by movie year, latest first.
+localMovies.sort(function(firstObj, secondObj) {
+  console.log('into sort');
+  return (new Date(secondObj.year)) - (new Date(firstObj.year));
+});
+
+//This creates a new array from the sorted array.
+localMovies.forEach(function(obj) {
+  console.log('into push');
+  movies.push(new Movie(obj));
+});
+
+//This runs the articleMaker method on each object.
+//And then appends the returned article to the section.
+movies.forEach(function (obj) {
+  console.log('into append');
+  $('#movies').append(obj.articleMaker());
+});
