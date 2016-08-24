055c879c6d4f0fbe2aeabbd539f42f665102c8d2
JOHN SHAFF <JOHNSHAFF@GMAIL.COM>
2016-08-23 18:18:55 -0700

    refactored loadAll with map instead of push

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,4 +1,3 @@
-// var movies = [];
 
 function Movie(newObj) {
   this.category = newObj.category;
@@ -14,34 +13,32 @@ Movie.allMovies = [];
 Movie.prototype.toHtml = function() {
   var source = $('#template').html();
   var templateRender = Handlebars.compile(source);
-  console.log('Current object', this);
   return templateRender(this);
 };
 
 Movie.loadAll = function(inputData) {
-  console.log(inputData);
-  inputData.sort(function(a,b) {
+  Movie.allMovies = inputData.sort(function(a,b) {
     return (new Date(b.year)) - (new Date(a.year));
-  }).forEach(function(ele) {
-    Movie.allMovies.push(new Movie(ele));
+  }).map(function (movieObject){
+    return new Movie(movieObject);
   });
 };
 
+// Movie.loadAll = function(inputData) {
+//   console.log(inputData);
+//   inputData.sort(function(a,b) {
+//     return (new Date(b.year)) - (new Date(a.year));
+//   }).forEach(function(ele) {
+//     Movie.allMovies.push(new Movie(ele));
+//   });
+//  };
+
 Movie.fetchAll = function() {
   if (localStorage.movieReviews) {
-    /* When our data is already in localStorage:
-    1. We can process and load it,
-    2. Then we can render the index page.  */
     Movie.loadAll(JSON.parse(localStorage.movieReviews));
     Movie.renderIndexPage();
   } else {
-    /* Without our localStorage in memory, we need to:
-    1. Retrieve our JSON file with $.getJSON
-      1.a Load our json data
-      1.b Store that data in localStorage so that we can skip the server call next time,
-      1.c And then render the index page.*/
     $.getJSON('scripts/movieRev.json', function(data) {
-      console.log('hey');
       localStorage.setItem('movieReviews', JSON.stringify(data));
       Movie.loadAll(data);
       Movie.renderIndexPage();
